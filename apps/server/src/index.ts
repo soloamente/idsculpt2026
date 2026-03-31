b6dce58ecef5ad38e69ddd8fcbabf3a720aa9f4d
Anselmo Diogo Guatta Vicente <soloartwork666@gmail.com>
2026-03-31 16:07:13 +0200

    initial commit

---
+++ apps/server/src/index.ts
@@ -0,0 +1,37 @@
+import { cors } from "@elysiajs/cors";
+import { createContext } from "@idsculpt/api/context";
+import { appRouter } from "@idsculpt/api/routers/index";
+import { auth } from "@idsculpt/auth";
+import { env } from "@idsculpt/env/server";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { Elysia } from "elysia";
+
+const app = new Elysia()
+  .use(
+    cors({
+      origin: env.CORS_ORIGIN,
+      methods: ["GET", "POST", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    }),
+  )
+  .all("/api/auth/*", async (context) => {
+    const { request, status } = context;
+    if (["POST", "GET"].includes(request.method)) {
+      return auth.handler(request);
+    }
+    return status(405);
+  })
+  .all("/trpc/*", async (context) => {
+    const res = await fetchRequestHandler({
+      endpoint: "/trpc",
+      router: appRouter,
+      req: context.request,
+      createContext: () => createContext({ context }),
+    });
+    return res;
+  })
+  .get("/", () => "OK")
+  .listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+  });
